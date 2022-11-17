de00c44eb5f6af891629e4491aa4d38b528302c2
neetikhare <neetikhare025@gmail.com>
2022-11-17 20:18:18 +0530

    done

---
+++ Welcome.java
@@ -0,0 +1,6 @@
+class Welcome {
+	public static void main(String[] args) {
+		System.out.println("Welcome! in this project.. It's my first java project");
+		System.out.println("We are validating automation through jenkins");
+	}
+}
